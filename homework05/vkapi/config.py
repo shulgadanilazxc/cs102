e2fe716a71858b942cc575517e011a83d30774bc
shulgadanilazxc <english.shulga@yandex.ru>
2021-01-29 18:29:35 +0300

    done

---
+++ homework05/vkapi/config.py
@@ -0,0 +1,7 @@
+# https://hackernoon.com/4-ways-to-manage-the-configuration-in-python-4623049e841b
+
+VK_CONFIG = {
+    "domain": "https://api.vk.com/method",
+    "access_token": "095122a2cb5a19ca5ea8911c662b39ad8640f25202f70af305b60569d0f90b851061dc7fc30e53dc2f3c6",
+    "version": "5.126",
+}
